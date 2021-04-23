9e676a2e83cc94f4fc10a78dcdab7097b8efb545
Mario Rosa <mariosergiodublin@gmail.com>
2021-04-23 19:24:21 +0100

    review code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
-const PORT = process.env.PORT || "5000";
+const PORT = process.env.PORT || "6000";
 
 dotenv.config();
 app.use(cors());
